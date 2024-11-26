8422376ef3144db58c324cea9f1881a6de4da930
Srishti1622 <shrishtibindki@gmail.com>
2024-11-26 22:35:56 +0530

    getting array from user

---
+++ Array/LeftRotateByKsteps.py
@@ -0,0 +1,9 @@
+# Given an integer array nums and a non-negative integer k, rotate the array to left by k steps
+
+# getting array size and elements from user
+arr=[]
+n=int(input("Enter the length of array: "))
+for i in range(n):
+    arr.append(int(input("Enter array element: ")))
+
+print("Array you provided: ",arr)
