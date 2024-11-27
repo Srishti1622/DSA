ae361df3390d549074a6fdb7f9ad4797b6d9291a
Srishti1622 <shrishtibindki@gmail.com>
2024-11-27 23:28:05 +0530

    2nd approach

--- Array/LeftRotateByKsteps.py
+++ Array/LeftRotateByKsteps.py
@@ -11,6 +11,20 @@ def WayOne(arr,k):
         arr[len(arr)-1]=temp
     return arr
 
+# 2nd Approach:
+def WayTwo(arr,k):
+    temp=[]
+    new_arr=[]
+    if k>len(arr):
+        k=k%len(arr)
+    for i in range(k):
+        temp.append(arr[i])
+    for i in range(k,len(arr)):
+        new_arr.append(arr[i])
+    for i in range(len(temp)):
+        new_arr.append(temp[i])
+    return new_arr
+
 # getting array size and elements from user
 arr=[]
 k=int(input("Enter the k step value to rotate: "))
@@ -21,5 +35,8 @@ for i in range(n):
 print("K steps: ",k)
 print("Array you provided: ",arr)
 # 1st way of doing it
-res1=WayOne(arr,k)
-print(f"Array after {k} rotates: {res1}")
+res1=WayOne(arr.copy(),k)
+print(f"Array after {k} rotates using 1st way: {res1}")
+# 2nd way of doing it
+res2=WayTwo(arr.copy(),k)
+print(f"Array after {k} rotates using 2nd way: {res2}")
