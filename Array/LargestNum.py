9706d188b98464b040a2fe882d75a4f0d1c7fe4b
Srishti1622 <shrishtibindki@gmail.com>
2024-11-29 23:25:14 +0530

    2nd approach

--- Array/LargestNum.py
+++ Array/LargestNum.py
@@ -1,9 +1,19 @@
 # Find the largest element in the given array
 
+# Reference: https://www.geeksforgeeks.org/problems/largest-element-in-array4009/0?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=largest-element-in-array
+
 # 1st Approach:
 def WayOne(arr):
     arr.sort()
     return arr[len(arr)-1]
+
+# 2nd Approach:
+def WayTwo(arr):
+    large=0
+    for i in range(len(arr)):
+        if arr[i]>large:
+            large=arr[i]
+    return large
 
 # getting array size and elements from user
 arr=[]
@@ -15,3 +25,6 @@ print("Array you provided: ",arr)
 # 1st way of doing it
 res1=WayOne(arr.copy())
 print("Largest element in given array using 1st approach is ",res1)
+# 2nd way of doing it
+res2=WayTwo(arr.copy())
+print("Largest element in given array using 2nd approach is ",res2)
