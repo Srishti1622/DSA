85382da63dc900233452e449cd652dd9ece325a6
Srishti1622 <shrishtibindki@gmail.com>
2024-11-29 22:44:31 +0530

    2nd approach

--- Array/CheckArraySorted.py
+++ Array/CheckArraySorted.py
@@ -5,6 +5,13 @@ def WayOne(arr1,arr2):
     arr2.sort()
     return arr1==arr2
 
+# 2nd Approach:
+def WayTwo(arr):
+    for i in range(1,len(arr)):
+        if arr[i-1]>arr[i]:
+            return False
+    return True
+
 # getting array size and elements from user
 arr=[]
 n=int(input("Enter the length of array: "))
@@ -15,3 +22,6 @@ print("Array you provided: ",arr)
 # 1st way of doing it
 res1=WayOne(arr,arr.copy())
 print("Checking given array is sorted or not using 1st approach is ",res1)
+# 2nd way of doing it
+res2=WayTwo(arr)
+print("Checking given array is sorted or not using 2nd approach is ",res2)
