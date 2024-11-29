acbc1a881e1ed9cbe08352163934317993aab20b
Srishti1622 <shrishtibindki@gmail.com>
2024-11-29 22:54:23 +0530

    3rd approach

--- Array/CheckArraySorted.py
+++ Array/CheckArraySorted.py
@@ -5,11 +5,19 @@ def WayOne(arr1,arr2):
     arr2.sort()
     return arr1==arr2
 
-# 2nd Approach:
+# 2nd Approach: Optimized
 def WayTwo(arr):
     for i in range(1,len(arr)):
         if arr[i-1]>arr[i]:
             return False
+    return True
+
+# 3rd Approach: Naive
+def WayThree(arr):
+    for i in range(len(arr)):
+        for j in range(i+1,len(arr)):
+            if arr[j]<arr[i]:
+                return False
     return True
 
 # getting array size and elements from user
@@ -25,3 +33,6 @@ print("Checking given array is sorted or not using 1st approach is ",res1)
 # 2nd way of doing it
 res2=WayTwo(arr)
 print("Checking given array is sorted or not using 2nd approach is ",res2)
+# 3rd way of doing it
+res3=WayThree(arr)
+print("Checking given array is sorted or not using 3rd approach is ",res3)
