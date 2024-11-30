69718812b37055cda244f0ffb1024a8680b111c5
Srishti1622 <shrishtibindki@gmail.com>
2024-11-30 14:51:36 +0530

    2nd approach

--- Array/RemoveDupSorted.py
+++ Array/RemoveDupSorted.py
@@ -1,8 +1,20 @@
 # Remove duplicate in-place elements from sorted array
 
+# Reference: https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+
 # 1st Approach:
 def WayOne(arr):
-    return set(arr)
+    return list(set(arr))
+
+# 2nd Approach:
+def WayTwo(arr):
+    new_arr=[arr[0]]
+    j=1
+    for i in range(len(arr)-1):
+        if arr[i]!=arr[j] and arr[j] not in new_arr:
+            new_arr.append(arr[j])
+        j+=1
+    return new_arr
 
 # getting array size and elements from user
 arr=[]
@@ -14,3 +26,6 @@ print("Array you provided: ",arr)
 # 1st way of doing it
 res1=WayOne(arr.copy())
 print("Remove duplicate element in given sorted array using 1st approach is ",res1)
+# 2nd way of doing it
+res2=WayTwo(arr.copy())
+print("Remove duplicate element in given sorted array using 2nd approach is ",res2)
