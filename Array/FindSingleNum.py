e6846de7210fc90c673416b64a7805b942c296a6
Srishti1622 <shrishtibindki@gmail.com>
2024-12-02 16:43:09 +0530

    1st approach

--- Array/FindSingleNum.py
+++ Array/FindSingleNum.py
@@ -2,6 +2,17 @@
 
 # Reference: https://leetcode.com/problems/single-number/description/
 
+# 1st Approach:
+def WayOne(arr):
+    for i in range(len(arr)):
+        flag=0
+        for j in range(len(arr)):
+            if arr[j]==arr[i]:
+                flag+=1
+        if flag==1:
+            return arr[i]
+    return -1
+
 # getting array size and elements from user
 arr=[]
 n=int(input("Enter the length of array: "))
@@ -11,4 +22,4 @@ for i in range(n):
 print("Array you provided: ",arr)
 # 1st way of doing it
 res1=WayOne(arr)
-print("Maximum Consecutive ones in given array using 1st approach is ",res1)
+print("Element that appears once in given array using 1st approach is ",res1)
