0bfebf15a9ce4c519b074f239e9587db9e0a69c4
Srishti1622 <shrishtibindki@gmail.com>
2024-12-02 17:31:29 +0530

    2nd approach

--- Array/SortedUnion.py
+++ Array/SortedUnion.py
@@ -1,8 +1,8 @@
 # Given two sorted arrays, arr1, and arr2 of size n and m. Find the union of two sorted arrays.
 
-# Reference: 
+# Reference: https://www.geeksforgeeks.org/problems/union-of-two-sorted-arrays-1587115621/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=union-of-two-sorted-arrays
 
-# 1st Approach:
+# 1st Approach: Works with both sorted or unsorted array
 def WayOne(arr1, arr2):
     s = set()
     union = []
@@ -14,6 +14,30 @@ def WayOne(arr1, arr2):
         union.append(num)
     union.sort()
     return union
+
+# 2nd Approach: works only with sorted arrays
+def WayTwo(arr1, arr2):
+    new_arr=[]
+    i=0
+    j=0
+    while(i<len(arr1) and j<len(arr2)):
+        if arr1[i]<=arr2[j]:
+            if len(new_arr)==0 or new_arr[-1]!=arr1[i]:
+                new_arr.append(arr1[i])
+            i=i+1
+        else:
+            if len(new_arr)==0 or new_arr[-1]!=arr2[j]:
+                new_arr.append(arr2[j])
+            j=j+1
+    while i < len(arr1): 
+        if new_arr[-1] != arr1[i]:
+            new_arr.append(arr1[i])
+        i += 1
+    while j < len(arr2): 
+        if new_arr[-1] != arr2[j]:
+            new_arr.append(arr2[j])
+        j += 1
+    return new_arr
 
 # getting 1st array size and elements from user
 arr1=[]
@@ -33,3 +57,6 @@ print("Array you provided: ",arr2)
 # 1st way of doing it
 res1=WayOne(arr1.copy(),arr2.copy())
 print("Union of two sorted array using 1st approach is ",res1)
+# 2nd way of doing it
+res2=WayTwo(arr1.copy(),arr2.copy())
+print("Union of two sorted array using 2nd approach is ",res2)
