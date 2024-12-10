6e4b6019506c765d9264dd2246fb4e78fda5bbec
Srishti1622 <shrishtibindki@gmail.com>
2024-12-10 16:04:27 +0530

    pattern 5c 5d

--- Patterns/pattern-5.py
+++ Patterns/pattern-5.py
@@ -25,3 +25,39 @@ for i in range(n):
         print(i+1,end=' ')
     print('\n')
 
+# pattern 5c: Star Pyramid-iii
+print('--------------------------------')
+print('Star Pyramid-iii')
+for i in range(n):
+    k=0
+    breakpoint=(2*i+1)/2
+    point=0
+    for j in range(2*i+1):
+        while(n-i-1>k):
+           k+=1
+           print(' ',end=' ')
+        if j<=breakpoint:
+            point+=1
+        else:
+            point-=1
+        print(point,end=' ')
+    print('\n')
+
+# pattern 5d: Alpha Star Pyramid-iv
+print('--------------------------------')
+print('Star Pyramid-iv')
+for i in range(n):
+    k=0
+    breakpoint=(2*i+1)//2
+    point='A'
+    while(n-i-1>k):
+        k+=1
+        print(' ',end=' ')
+    for j in range(2*i+1):
+        print(point,end=' ')
+        if j<breakpoint:
+            point=chr(ord(point)+1)
+        else:
+            point=chr(ord(point)-1)
+    print('\n')
+
