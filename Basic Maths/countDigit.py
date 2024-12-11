a9d3624e290aff59853899925e31caeca0d25d60
Srishti1622 <shrishtibindki@gmail.com>
2024-12-11 20:46:01 +0530

    3rd approach

--- Basic Maths/countDigit.py
+++ Basic Maths/countDigit.py
@@ -2,6 +2,8 @@
 
 # Reference: 
 
+import math
+
 # 1st Approach: Brute-force
 def WayOne(num):
     str_num=str(num)
@@ -15,11 +17,18 @@ def WayTwo(num):
         num=num//10
     return count
 
+# 3rd Approach: Optimized
+def WayThree(num):
+    return int(math.log10(num)+1)
+
 # getting the number from user
 num=int(input("Enter the number: "))
 # 1st way of doing this
 res1=WayOne(num)
-print(f"The length of given number is : {res1}")
+print(f"The length of given number using 1st way is : {res1}")
 # 2nd way of doing this
 res2=WayTwo(num)
-print(f"The length of given number is : {res2}")
+print(f"The length of given number using 2nd way is : {res2}")
+# 3rd way of doing this
+res3=WayThree(num)
+print(f"The length of given number using 3rd way is : {res3}")
