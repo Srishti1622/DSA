d1a460c1032e6a6b351f94b520172ee9b82d7b66
Srishti1622 <shrishtibindki@gmail.com>
2024-12-11 20:37:52 +0530

    1st approach

--- Basic Maths/countDigit.py
+++ Basic Maths/countDigit.py
@@ -3,6 +3,19 @@
 # Reference: 
 
 # 1st Approach: Brute-force
+def WayOne(num):
+    str_num=str(num)
+    return len(str_num)
+
+# 2nd Approach: Brute-force
+def WayTwo(num):
+    
 
 # getting the number from user
 num=int(input("Enter the number: "))
+# 1st way of doing this
+res1=WayOne(num)
+print(f"The length of given number is : {res1}")
+# 2nd way of doing this
+res2=WayTwo(num)
+print(f"The length of given number is : {res2}")
