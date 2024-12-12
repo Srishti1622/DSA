9552c066d1637cbc4f9ad0a4ab5f2f836fcc80b9
Srishti1622 <shrishtibindki@gmail.com>
2024-12-12 14:40:22 +0530

    2nd approach

--- Basic Maths/reverseNum.py
+++ Basic Maths/reverseNum.py
@@ -11,8 +11,20 @@ def WayOne(num):
     r_reverse=''.join(reversed(str_num))
     return int(r_reverse)
 
+# 2nd Approach: Brute-force
+def WayTwo(num):
+    r_reverse=0
+    while(num>0):
+        rem=num%10
+        r_reverse=r_reverse*10+rem
+        num=num//10
+    return r_reverse
+
 # getting the number from user
 num=int(input("Enter the number: "))
 # 1st way of doing this
 res1=WayOne(num)
 print(f"The length of given number using 1st way is : {res1}")
+# 2nd way of doing this
+res2=WayTwo(num)
+print(f"The length of given number using 2nd way is : {res2}")
