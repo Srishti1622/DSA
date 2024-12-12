12d96542044f595ebd8be5a3b6ca589406c23112
Srishti1622 <shrishtibindki@gmail.com>
2024-12-12 15:07:36 +0530

    2nd approach

--- Basic Maths/palindrome.py
+++ Basic Maths/palindrome.py
@@ -8,8 +8,21 @@ def WayOne(num):
     num1=int(''.join(reversed(str(num))))
     return num==num1
 
+# 2nd Approach: 
+def WayTwo(num):
+    num1=num
+    revNum=0
+    while(num1>0):
+        rem=num1%10
+        revNum=revNum*10+rem
+        num1=num1//10
+    return num==revNum
+
 # getting number from user
 num=int(input("Enter number to be checked: "))
 # 1st way of doing it
 res1=WayOne(num)
 print(f"The given number is palidrome or not: {res1}")
+# 2nd way of doing it
+res2=WayTwo(num)
+print(f"The given number is palidrome or not: {res2}")
